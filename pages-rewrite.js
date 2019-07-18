dfb4093de77de43f44e649e4a2d368d7afe68f3d
alexandra-nagy <alexandra.nagy@aol.com>
2019-07-18 23:56:00 +0200

    Update pages-rewrite.js

--- pages-rewrite.js
+++ pages-rewrite.js
@@ -31,103 +31,103 @@ let lastScreenshot = Promise.resolve();
 function promiseFinally(p, callback)
 {
   return p.then(
-    callback,
-    err => Promise.resolve(callback()).then(() =>
-      Promise.reject(err)
-    )
-  );
+        callback,
+        err => Promise.resolve(callback()).then(() =>
+            Promise.reject(err)
+        )
+    );
 }
 
 function closeWindow(driver, goTo, returnTo, callback)
 {
   return promiseFinally(
-    driver.switchTo().window(goTo).then(() =>
-      promiseFinally(
-        new Promise(resolve => resolve(callback && callback())),
-        () => driver.close()
-      )
-    ),
-    () => driver.switchTo().window(returnTo)
-  );
+        driver.switchTo().window(goTo).then(() =>
+            promiseFinally(
+                new Promise(resolve => resolve(callback && callback())),
+                () => driver.close()
+            )
+        ),
+        () => driver.switchTo().window(returnTo)
+    );
 }
 
 function takeScreenshot(element)
 {
-  // It would be preferable if we could use WebElement.takeScreenshot(),
-  // but it's not supported on Chrome, and produces incorrect output when
-  // called repeatedly, on Firefox >=58 or when using geckodriver >=1.13.
-  // So as a workaround, we scroll to the position of the element, take a
-  // screenshot of the viewport and crop it to the element's size and position.
+    // It would be preferable if we could use WebElement.takeScreenshot(),
+    // but it's not supported on Chrome, and produces incorrect output when
+    // called repeatedly, on Firefox >=58 or when using geckodriver >=1.13.
+    // So as a workaround, we scroll to the position of the element, take a
+    // screenshot of the viewport and crop it to the element's size and position.
   lastScreenshot = Promise.all([element.getRect(),
-                                lastScreenshot]).then(([rect]) =>
-    element.getDriver().executeScript(`
+                                  lastScreenshot]).then(([rect]) =>
+        element.getDriver().executeScript(`
       window.scrollTo(${rect.x}, ${rect.y});
       return [window.scrollX, window.scrollY];
     `).then(result =>
-    {
+        {
       let x = rect.x - result[0];
-      let y = rect.y - result[1];
+            let y = rect.y - result[1];
 
-      return element.getDriver().takeScreenshot()
-        .then(s => Jimp.read(Buffer.from(s, "base64")))
-        .then(img => img.crop(x, y, rect.width, rect.height).bitmap);
-    })
-  );
-  return lastScreenshot;
+            return element.getDriver().takeScreenshot()
+                .then(s => Jimp.read(Buffer.from(s, "base64")))
+                .then(img => img.crop(x, y, rect.width, rect.height).bitmap);
+        })
+    );
+    return lastScreenshot;
 }
 
 function getSections(driver)
 {
-  return driver.findElements(By.css("section")).then(elements =>
-    Promise.all(elements.map(e =>
-      Promise.all([
-        e.findElement(By.css("h2")).catch(() => null),
-        e.findElement(By.className("testcase-container")).catch(() => null),
-        e.findElements(By.css("pre"))
-      ])
-    ))
-  ).then(sections => sections.filter(
-    ([title, demo, filters]) => title && demo && filters.length > 0
-  ));
+    return driver.findElements(By.css("section")).then(elements =>
+        Promise.all(elements.map(e =>
+            Promise.all([
+                e.findElement(By.css("h2")).catch(() => null),
+                e.findElement(By.className("testcase-container")).catch(() => null),
+                e.findElements(By.css("pre"))
+            ])
+        ))
+    ).then(sections => sections.filter(
+        ([title, demo, filters]) => title && demo && filters.length > 0
+    ));
 }
 
 it("Test pages", function()
 {
-  return this.driver.navigate().to(TEST_PAGES_URL).then(() =>
-    this.driver.findElements(By.css(".site-pagelist a"))
-  ).then(elements =>
-    Promise.all(elements.map(elem => Promise.all([elem.getAttribute("class"),
-                                                  elem.getAttribute("href"),
-                                                  elem.getText()])))
-  ).then(urls =>
-  {
-    let p1 = Promise.resolve();
-    for (let [elemClass, url, pageTitle] of urls)
+    return this.driver.navigate().to(TEST_PAGES_URL).then(() =>
+        this.driver.findElements(By.css(".site-pagelist a"))
+    ).then(elements =>
+        Promise.all(elements.map(elem => Promise.all([elem.getAttribute("class"),
+            elem.getAttribute("href"),
+            elem.getText()])))
+    ).then(urls =>
     {
-      let onlineTestCase = elemClass &&
-                           elemClass.split(/\s+/).includes("online");
-      if (SKIP_ONLINE_TESTS && onlineTestCase)
-        continue;
+        let p1 = Promise.resolve();
+        for (let [elemClass, url, pageTitle] of urls)
+        {
+            let onlineTestCase = elemClass &&
+                elemClass.split(/\s+/).includes("online");
+            if (SKIP_ONLINE_TESTS && onlineTestCase)
+                continue;
 
-      let browser = this.test.parent.title.replace(/\s.*$/, "");
-      if (// https://issues.adblockplus.org/ticket/6917
-          pageTitle == "$subdocument" && browser == "Firefox" ||
-          // Chromium doesn't support Flash
-          pageTitle.startsWith("$object") && browser == "Chromium" ||
-          // Chromium 63 doesn't have user stylesheets (required to
-          // overrule inline styles) and doesn't run content scripts
-          // in dynamically written documents.
-          this.test.parent.title == "Chromium (oldest)" &&
-          (pageTitle == "Inline style !important" ||
-           pageTitle == "Anonymous iframe document.write()"))
-        continue;
+            let browser = this.test.parent.title.replace(/\s.*$/, "");
+            if (// https://issues.adblockplus.org/ticket/6917
+                pageTitle === "$subdocument" && browser === "Firefox" ||
+                // Chromium doesn't support Flash
+                pageTitle.startsWith("$object") && browser === "Chromium" ||
+                // Chromium 63 doesn't have user stylesheets (required to
+                // overrule inline styles) and doesn't run content scripts
+                // in dynamically written documents.
+                this.test.parent.title === "Chromium (oldest)" &&
+                (pageTitle === "Inline style !important" ||
+                    pageTitle === "Anonymous iframe document.write()"))
+                continue;
 
-      p1 = p1.then(() =>
-        this.driver.navigate().to(url)
-      ).then(() =>
-        Promise.all([
-          getSections(this.driver),
-          this.driver.executeScript(`
+            p1 = p1.then(() =>
+                this.driver.navigate().to(url)
+            ).then(() =>
+                Promise.all([
+                    getSections(this.driver),
+                    this.driver.executeScript(`
             let documents = [document];
             while (documents.length > 0)
             {
@@ -143,29 +143,29 @@ it("Test pages", function()
               }
             }
           `)
-        ])
-      ).then(([sections]) =>
-        Promise.all(sections.map(([title, demo, filters]) =>
-          Promise.all([
-            title.getAttribute("textContent").then(testTitle =>
-              `${pageTitle.trim()} - ${testTitle.trim()}`
-            ),
-            takeScreenshot(demo),
-            Promise.all(filters.map(elem => elem.getAttribute("textContent")))
-          ])
-        ))
-      ).then(testCases =>
-      {
-        let p2 = Promise.resolve();
-        for (let i = 0; i < testCases.length; i++)
-        {
-          let [title, expectedScreenshot, filters] = testCases[i];
-          let description = ["", "Test case: " + title, url].join("\n       ");
+                ])
+            ).then(([sections]) =>
+                Promise.all(sections.map(([title, demo, filters]) =>
+                    Promise.all([
+                        title.getAttribute("textContent").then(testTitle =>
+                            `${pageTitle.trim()} - ${testTitle.trim()}`
+                        ),
+                        takeScreenshot(demo),
+                        Promise.all(filters.map(elem => elem.getAttribute("textContent")))
+                    ])
+                ))
+            ).then(testCases =>
+            {
+                let p2 = Promise.resolve();
+                for (let i = 0; i < testCases.length; i++)
+                {
+                    let [title, expectedScreenshot, filters] = testCases[i];
+                    let description = ["", "Test case: " + title, url].join("\n       ");
 
-          p2 = p2.then(() =>
-            this.driver.navigate().to(this.origin + "/options.html")
-          ).then(() =>
-            this.driver.executeAsyncScript(`
+                    p2 = p2.then(() =>
+                        this.driver.navigate().to(this.origin + "/options.html")
+                    ).then(() =>
+                        this.driver.executeAsyncScript(`
               let filters = arguments[0];
               let callback = arguments[arguments.length - 1];
               browser.runtime.sendMessage({type: "subscriptions.get",
@@ -180,95 +180,95 @@ it("Test pages", function()
                                              text: filters})
               ).then(errors => callback(errors[0]), callback);
             `, filters.join("\n"))
-          ).then(error =>
-          {
-            if (error)
-              throw error;
-            return this.driver.navigate().to(url);
-          }).then(() =>
-          {
-            if (pageTitle.startsWith("$popup"))
-            {
-              return getSections(this.driver).then(sections =>
-                sections[i][1].findElement(By.css("a[href],button")).click()
-              ).then(() =>
-                this.driver.sleep(500)
-              ).then(() =>
-                this.driver.getAllWindowHandles()
-              ).then(handles =>
-              {
-                if (pageTitle == "$popup - Exception")
-                {
-                  assert.equal(handles.length, 3,
-                               "Popup is whitelisted" + description);
-                  return closeWindow(this.driver, handles[2], handles[1]);
+                    ).then(error =>
+                    {
+                        if (error)
+                            throw error;
+                        return this.driver.navigate().to(url);
+                    }).then(() =>
+                    {
+                        if (pageTitle.startsWith("$popup"))
+                        {
+                            return getSections(this.driver).then(sections =>
+                                sections[i][1].findElement(By.css("a[href],button")).click()
+                            ).then(() =>
+                                this.driver.sleep(500)
+                            ).then(() =>
+                                this.driver.getAllWindowHandles()
+                            ).then(handles =>
+                            {
+                                if (pageTitle === "$popup - Exception")
+                                {
+                                    assert(handles.length, 3,
+                                        "Popup is whitelisted" + description);
+                                    return closeWindow(this.driver, handles[2], handles[1]);
+                                }
+
+                                assert(handles.length, 2,
+                                    "Popup is blocked" + description);
+                            });
+                        }
+
+                        let checkTestCase = () =>
+                            getSections(this.driver).then(sections =>
+                                this.driver.wait(() =>
+                                    takeScreenshot(sections[i][1]).then(screenshot =>
+                                        screenshot.width === expectedScreenshot.width &&
+                                        screenshot.height === expectedScreenshot.height &&
+                                        screenshot.data.compare(expectedScreenshot.data) === 0
+                                    ), 1000
+                                ).catch(e =>
+                                {
+                                    if (e instanceof TimeoutError)
+                                        e = new Error("Screenshots don't match" + description);
+                                    throw e;
+                                })
+                            );
+
+                        // Sometimes on Firefox there is a delay until the added
+                        // filters become effective. So if the test case fails once,
+                        // we reload the page and try once again.
+                        return checkTestCase().catch(() =>
+                            this.driver.navigate().refresh().then(checkTestCase)
+                        );
+                    });
                 }
-
-                assert.equal(handles.length, 2,
-                             "Popup is blocked" + description);
-              });
-            }
-
-            let checkTestCase = () =>
-              getSections(this.driver).then(sections =>
-                this.driver.wait(() =>
-                  takeScreenshot(sections[i][1]).then(screenshot =>
-                    screenshot.width == expectedScreenshot.width &&
-                    screenshot.height == expectedScreenshot.height &&
-                    screenshot.data.compare(expectedScreenshot.data) == 0
-                  ), 1000
-                ).catch(e =>
-                {
-                  if (e instanceof TimeoutError)
-                    e = new Error("Screenshots don't match" + description);
-                  throw e;
-                })
-              );
-
-            // Sometimes on Firefox there is a delay until the added
-            // filters become effective. So if the test case fails once,
-            // we reload the page and try once again.
-            return checkTestCase().catch(() =>
-              this.driver.navigate().refresh().then(checkTestCase)
-            );
-          });
+                return p2;
+            });
         }
-        return p2;
-      });
-    }
-    return p1;
-  });
+        return p1;
+    });
 });
 
 it("subscribe link", function()
 {
-  return this.driver.navigate().to(TEST_PAGES_URL).then(() =>
-    this.driver.findElement(By.id("subscribe-button")).click()
-  ).then(() =>
-    this.driver.wait(() =>
-      this.driver.getAllWindowHandles().then(handles =>
-        handles.length > 2 ? handles : null
-      ), 3000
-    )
-  ).then(handles =>
-    closeWindow(this.driver, handles[2], handles[1], () =>
-      this.driver.wait(until.ableToSwitchToFrame(0), 1000).then(() =>
-        this.driver.wait(
-          until.elementLocated(By.id("dialog-content-predefined")), 1000
-        )
-      ).then(dialog =>
+    return this.driver.navigate().to(TEST_PAGES_URL).then(() =>
+        this.driver.findElement(By.id("subscribe-button")).click()
+    ).then(() =>
         this.driver.wait(() =>
-          Promise.all([
-            dialog.isDisplayed(),
-            dialog.findElement(By.css("h3")).getText()
-          ]).then(([displayed, title]) =>
-            displayed && title == "ABP Testcase Subscription"
-          ), 1000, "dialog shown"
-        ).then(() =>
-          dialog.findElement(By.css("button")).click()
+            this.driver.getAllWindowHandles().then(handles =>
+                handles.length > 2 ? handles : null
+            ), 3000
         )
-      ).then(() =>
-        this.driver.executeAsyncScript(`
+    ).then(handles =>
+        closeWindow(this.driver, handles[2], handles[1], () =>
+            this.driver.wait(until.ableToSwitchToFrame(0), 1000).then(() =>
+                this.driver.wait(
+                    until.elementLocated(By.id("dialog-content-predefined")), 1000
+                )
+            ).then(dialog =>
+                this.driver.wait(() =>
+                    Promise.all([
+                        dialog.isDisplayed(),
+                        dialog.findElement(By.css("h3")).getText()
+                    ]).then(([displayed, title]) =>
+                        displayed && title === "ABP Testcase Subscription"
+                    ), 1000, "dialog shown"
+                ).then(() =>
+                    dialog.findElement(By.css("button")).click()
+                )
+            ).then(() =>
+                this.driver.executeAsyncScript(`
           let callback = arguments[arguments.length - 1];
           browser.runtime.sendMessage({type: "subscriptions.get",
                                        ignoreDisabled: true,
@@ -281,12 +281,12 @@ it("subscribe link", function()
             err => callback([null, err])
           );
         `)
-      ).then(([added, err]) =>
-      {
-        if (err)
-          throw err;
-        assert.ok(added, "subscription added");
-      })
-    )
-  );
+            ).then(([added, err]) =>
+            {
+                if (err)
+                    throw err;
+                assert.ok(added, "subscription added");
+            })
+        )
+    );
 });
